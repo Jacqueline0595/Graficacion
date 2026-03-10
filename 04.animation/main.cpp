5f203132e8c7095f01764f4b897200da3f1f2057
Jacqueline0595 <janellejackelinegomezbenitez@gmail.com>
2026-03-10 09:11:11 -0600

    Ejercicio clase 10 de marzo

--- 04.animation/main.cpp
+++ 04.animation/main.cpp
@@ -13,31 +13,22 @@ int main()
     // mover punto a traves de una trayectoria linea
 
     // punto a mover
-    Vertex v1(-3.1, 4.7, -0.3);
-    Vertex v2(0.1, -4.2, 11.8);
+    Vertex P1(-3.1, 4.7, -0.3);
+    Vertex P2(0.1, -4.2, 11.8);
 
-    // paso 1 trasladar v2 al origen
-    arma::Mat<float> T1 = an.T(-v2.get_x(), -v2.get_y(), -v2.get_z());
-    
-    // cout << "v1p: " << v1p.t() << endl;
-    // cout << "v2p: " << v2p.t() << endl;
-
-    // paso 2 llevamos al plano YZ
-    arma::Mat<float> Ry2 = an.Ry(-14.81);
-    
-    // cout << "v1pp: " << v1pp.t() << endl;
-    // cout << "v2pp: " << v2pp.t() << endl;
-
-    // paso 3 rotar en x para que v1pp quede sobre el eje z
-    arma::Mat<float> Rx3 = an.Rx(-35.41);
-
-    // matriz compuesta
-    arma::Mat<float> MC = Rx3 * Ry2 * T1;
-
-    arma::Mat<float> v1p = MC * v1.h();
-    arma::Mat<float> v2p = MC * v2.h();
-
-    cout << "v1p: " << v1p.t() << endl;
-    cout << "v2p: " << v2p.t() << endl; 
+    // paso 1
+    arma::Mat<float> T = an.T(-P1.get_x(), -P1.get_y(), -P1.get_z());
+    // paso 2 
+    float D1 = sqrt(powf(P2.get_z()-P1.get_z(), 2) + powf(P2.get_x()-P1.get_x(), 2));
+    arma::Mat<float> Ry = { { P2.get_z()-P1.get_z() / D1, 0, P2.get_x()-P1.get_x() / D1, 0},
+                            { 0, 1, 0, 0},
+                            { -(P2.get_x()-P1.get_x()) / D1, 0, (P2.get_z()-P1.get_z()) / D1, 0},
+                            { 0, 0, 0, 1} };
+    // paso 3
+    float D2 = sqrt(powf(P2.get_x()-P1.get_x(), 2) + powf(P2.get_y()-P1.get_y(), 2) + powf(P2.get_z()-P1.get_z(), 2));
+    arma::Mat<float> Rx = { { 1, 0, 0, 0},
+                            { 0, (P2.get_z()-P1.get_z()) / D2, -(P2.get_y()-P1.get_y()) / D2, 0},
+                            { 0, (P2.get_y()-P1.get_y()) / D2, (P2.get_z()-P1.get_z()) / D2, 0},
+                            { 0, 0, 0, 1} };
     return 0;
 }
