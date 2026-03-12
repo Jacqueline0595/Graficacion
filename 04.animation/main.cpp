ba53b98352aa0e22c31d08cf6c754d433864c89c
Jacqueline0595 <janellejackelinegomezbenitez@gmail.com>
2026-03-12 08:39:41 -0600

    Ejercicio de clase 12 - Marzo. Pero que pedo con el error que sale al compilar

--- 04.animation/main.cpp
+++ 04.animation/main.cpp
@@ -10,29 +10,39 @@ using namespace arma;
 int main()
 {
     Animation an;
-    // mover punto a traves de una trayectoria linea
-
+    
     // punto a mover
-    Vertex P1(-3.1, 4.7, -0.3);
-    Vertex P2(0.1, -4.2, 11.8);
+    Vertex P1(-0.34, 9.8, 0.0); 
+    Vertex P2(8.74, -34.1, -5.29);
 
     // paso 1
-    arma::Mat<float> T = an.T(-P1.get_x(), -P1.get_y(), -P1.get_z());
+
+    arma::Mat<float> T1 = an.T(-P1.get_x(), -P1.get_y(), -P1.get_z());
     // paso 2 
     float D1 = sqrt(powf(P2.get_z()-P1.get_z(), 2) + powf(P2.get_x()-P1.get_x(), 2));
-    arma::Mat<float> Ry = { { P2.get_z()-P1.get_z() / D1, 0, P2.get_x()-P1.get_x() / D1, 0},
+    arma::Mat<float> Ry2 = { { P2.get_z()-P1.get_z() / D1, 0, P2.get_x()-P1.get_x() / D1, 0},
                             { 0, 1, 0, 0},
-                            { -(P2.get_x()-P1.get_x()) / D1, 0, (P2.get_z()-P1.get_z()) / D1, 0},
+                            { (P2.get_x()-P1.get_x()) / D1, 0, (P2.get_z()-P1.get_z()) / D1, 0},
                             { 0, 0, 0, 1} };
     // paso 3
-    float D2 = sqrt(powf(P2.get_x()-P1.get_x(), 2) + powf(P2.get_y()-P1.get_y(), 2) + powf(P2.get_z()-P1.get_z(), 2));
-    arma::Mat<float> Rx = { { 1, 0, 0, 0},
-                            { 0, (P2.get_z()-P1.get_z()) / D2, -(P2.get_y()-P1.get_y()) / D2, 0},
-                            { 0, (P2.get_y()-P1.get_y()) / D2, (P2.get_z()-P1.get_z()) / D2, 0},
+    float D2 = sqrt( powf(P2.get_x()-P1.get_x(), 2) + powf(P2.get_y()-P1.get_y(), 2) + powf(P2.get_z()-P1.get_z(), 2) );
+    arma::Mat<float> Rx3 = { { 1, 0, 0, 0},
+                            { 0, D1/D2, -(P2.get_y()-P1.get_y()) / D2, 0},
+                            { 0, (P2.get_y()-P1.get_y()) / D2, D1 / D2, 0},
                             { 0, 0, 0, 1} };
 
-    cout << "T: " << T << endl;
-    cout << "Ry: " << Ry << endl;
-    cout << "Rx: " << Rx << endl;
+    // regresar
+    arma::Mat<float> Rx5 = Rx3.i();
+    arma::Mat<float> Ry6 = Ry2.i();
+    arma::Mat<float> T7 = T1.i();
+                            
+    arma::Mat<float> MC = T7 * Ry6 * Rx5 * Rx3 * Ry2 * T1;
+
+    arma::Mat<float> p1p = MC * P1.h();
+    arma::Mat<float> p2p = MC * P2.h();
+
+    cout << p1p << endl;
+    cout << p2p << endl;
+
     return 0;
 }
