2e77ee21a6cd2ade4db8e853f61c8d22ad8e3425
Jacqueline0595 <janellejackelinegomezbenitez@gmail.com>
2026-04-12 17:50:55 -0600

    Metodos faltantes completos

--- 59.proyect/cpp/Archer.cpp
+++ 59.proyect/cpp/Archer.cpp
@@ -7,7 +7,7 @@ Archer::Archer(float x, float y, float z)
     this->position.set_z(z);
 
     this->arrow_pos.set_x(x);
-    this->arrow_pos.set_y(y + 0.1);
+    this->arrow_pos.set_y(y);
     this->arrow_pos.set_z(z);
 
     this->angel = 0.0; 
@@ -75,16 +75,76 @@ void Archer::main_loop()
 void Archer::shootBow()
 {
     // Disparar la flecha a traves de una trayectoria curva (bezier)
+    cout << "Flecha disparada con Bezier" << endl;
+
+    Animation an;
+
+    Vertex P1 = this->arrow_pos;
+
+    float rangle = this->angel * M_PI / 180.0; // a radianes
+
+    Vertex P2(
+        arrow_pos.get_x() + this->force,
+        arrow_pos.get_y() + (1 - cos(rangle)),
+        arrow_pos.get_z()
+    );
+
+    Vertex P3(
+        arrow_pos.get_x() + (this->force * 2),
+        P2.get_y(),
+        arrow_pos.get_z()
+    );
+
+    Vertex P4(
+        arrow_pos.get_x() + (this->force * 3),
+        0,
+        arrow_pos.get_z()
+    );
+
+    // Generar trayectoria con Bezier
+    this->arrow_trayectory = an.bezier(P1, P2, P3, P4, 0.1);
+
+    for (Vertex bt : this->arrow_trayectory)
+    {
+        vector<Vertex> transformed_vertices = {};
+
+        arma::Mat<float> T1 = an.T(bt.get_x(), bt.get_y(), bt.get_z());
+
+        for (Vertex v : this->arrow.get_vertices())
+        {
+            arma::Col<float> vt = T1 * v.h();
+
+            Vertex vtp(
+                vt.at(0,0)/vt.at(3,0),
+                vt.at(1,0)/vt.at(3,0),
+                vt.at(2,0)/vt.at(3,0)
+            );
+
+            transformed_vertices.push_back(vtp);
+        }
+    }
 }
 
 void Archer::set_angel(float inc)
 {
     // Aumentar o disminuir el ángulo de disparo
+    this->angel += inc;
+
+    if (this->angel > 85.0)
+        this->angel = 85;
+    else if (this->angel < 0.0)
+        this->angel = 0;
 }
 
 void Archer::set_force(float inc)
 {
     // Aumentar o disminuir la fuerza de disparo
+    this->force += inc;
+
+    if (this->force > 3.0)
+        this->force = 3;
+    else if (this->force < 0.1)
+        this->force = 0.1;
 }
 
 void Archer::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
