b9588620185cc6b41240280753a30331dfdfc254
Jacqueline0595 <janellejackelinegomezbenitez@gmail.com>
2026-05-13 22:41:24 -0600

    Clase Vertex

--- 10.proyect/cpp/Vertex.cpp
+++ 10.proyect/cpp/Vertex.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <armadillo>
 #include "../include/Vertex.h"
 
 using namespace std;
@@ -8,11 +9,20 @@ Vertex::Vertex(float x, float y, float z)
     this->x = x;
     this->y = y;
     this->z = z;
+    this->nx = 0.0;
+    this->ny = 0.0;
+    this->nz = 0.0;
 }
 
 void Vertex::print()
 {
     cout << "(" << this->x << ", " << this->y << ", " << this->z << ")";
+}
+
+Vertex Vertex::operator+(Vertex op2)
+{
+    Vertex v((this->x + op2.x), (this->y + op2.y), (this->z + op2.z));
+    return(v);
 }
 
 Vertex Vertex::operator-(Vertex op2)
@@ -24,12 +34,6 @@ Vertex Vertex::operator-(Vertex op2)
 Vertex Vertex::operator*(float op2)
 {
     Vertex v((this->x * op2), (this->y * op2), (this->z * op2));
-    return(v);
-}
-
-Vertex Vertex::operator+(Vertex op2)
-{
-    Vertex v((this->x + op2.x), (this->y + op2.y), (this->z + op2.z));
     return(v);
 }
 
@@ -65,4 +69,43 @@ arma::Col<float> Vertex::h()
 {
     arma::Col<float> ch = {this->x, this->y, this->z, 1};
     return(ch);
+}
+
+float Vertex::get_nx()
+{
+    return(this->nx);
+}
+
+float Vertex::get_ny()
+{
+    return(this->ny);
+}
+
+float Vertex::get_nz()
+{
+    return(this->nz);
+}
+
+void Vertex::set_nx(float nx)
+{
+    this->nx = nx;
+}
+
+void Vertex::set_ny(float ny)
+{
+    this->ny = ny;
+}
+
+void Vertex::set_nz(float nz)
+{
+    this->nz = nz;
+}
+
+void Vertex::normal_average()
+{
+    arma::vec3 nrm = {this->nx, this->ny, this->nz};
+    arma::vec3 nnrm = arma::normalise(nrm);
+    this->nx = nnrm.at(0);
+    this->ny = nnrm.at(1);
+    this->nz = nnrm.at(2);
 }
--- 10.proyect/include/Vertex.h
+++ 10.proyect/include/Vertex.h
@@ -4,10 +4,13 @@
 #pragma once
 #include <armadillo>
 
+using namespace std;
+
 class Vertex
 {
 private:
-    float x, y, z;
+    float x, y, z, w;
+    float nx, ny, nz;
 public:
     Vertex() {};
     Vertex(float x, float y, float z); 
@@ -22,6 +25,16 @@ public:
     void set_x(float x);
     void set_y(float y);
     void set_z(float z);
+
+    float get_nx();
+    float get_ny();
+    float get_nz();
+
+    void set_nx(float x);
+    void set_ny(float y);
+    void set_nz(float z);
+
+    void normal_average();
 
     arma::Col<float> h();
 };
